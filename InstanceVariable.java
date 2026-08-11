957c2a6999c5a84202147968ee49a9347aa6988a
KartikReddy1 <skr.somu387@gmail.com>
2026-08-12 01:36:15 +0530

    Added InstanceVariable.java

---
+++ InstanceVariable.java
@@ -0,0 +1,10 @@
+public class InstanceVariable {
+    int id = 100;
+    public static void main(String[] args) {
+        InstanceVariable instanceVariable = new InstanceVariable();
+        
+        System.out.println(instanceVariable.id);
+        
+    }
+    
+}
