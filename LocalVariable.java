b2ad8bffd80f0633481af02620b504f0888c2495
KartikReddy1 <skr.somu387@gmail.com>
2026-08-12 02:03:39 +0530

    Added LocalVariable

---
+++ LocalVariable.java
@@ -0,0 +1,11 @@
+public class LocalVariable {
+    public static void main(String[] args) {
+        String name ="Karthik";
+        int weight = 75;
+        Float height = 6.2f;
+        System.out.println(name);
+        System.out.println(weight);
+        System.out.println(height);
+    }
+    
+}
