cb4e1c0aa947d679855ac25c5c8feb4280c7b0bb
KartikReddy1 <skr.somu387@gmail.com>
2026-08-12 01:53:41 +0530

    Added StaticVariable

---
+++ StaticVariable.java
@@ -0,0 +1,11 @@
+public class StaticVariable {
+   static int id = 100;
+   String name = "Karthik";
+    public static void main(String[] args) {
+        
+        System.out.println(id);
+        System.out.println(new StaticVariable().name);
+        
+    }
+    
+}
