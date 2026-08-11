b8a5c295edfc2db9a8c272ebff3bbcf5df4350b6
KartikReddy1 <skr.somu387@gmail.com>
2026-08-11 23:51:57 +0530

    Added Range.java

--- Range.java
+++ Range.java
@@ -1,8 +1,10 @@
 public class Range {
     public static void main(String[] args) {
-        Byte b = 10;
-        System.out.println(Byte.MIN_VALUE);
-        System.out.println(Byte.MAX_VALUE);
+       // Byte b = 10;
+       int i=1000000000;
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
 
 
     }
