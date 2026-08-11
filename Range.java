7f98eb3b2df9b8144e39a39cea2ead3a0d8fe1b7
KartikReddy1 <skr.somu387@gmail.com>
2026-08-12 00:03:23 +0530

    Updated Range.java

--- Range.java
+++ Range.java
@@ -1,7 +1,14 @@
 public class Range {
     public static void main(String[] args) {
-       // Byte b = 10;
+       Byte b = 10;
+       Short s = 250;
        int i=1000000000;
+       long l = 9876598765l;
+       float f = 3.14f;
+       double d = 89.3456;
+       boolean b1 = true;
+       boolean b2 = false;
+       char c= 's' ;
 
         System.out.println(Integer.MIN_VALUE);
         System.out.println(Integer.MAX_VALUE);
