a4b90d7bc7f2f35965109dbb6cc880f39425dbd1
KartikReddy1 <skr.somu387@gmail.com>
2026-08-12 02:18:52 +0530

    Added Parameter

---
+++ parameter.java
@@ -0,0 +1,10 @@
+public class parameter {
+     public static void main(String[] args) {
+    test1(70);
+    }
+    public static void test1(int height) {
+        height =50;
+        System.out.println(height);
+        
+    }
+}
