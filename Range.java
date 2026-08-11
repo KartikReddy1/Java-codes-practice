c29c8cfa7666392d064ffc014f88bee0ff5a449a
KartikReddy1 <skr.somu387@gmail.com>
2026-08-11 23:38:47 +0530

    Added Range.java

---
+++ Range.java
@@ -0,0 +1,9 @@
+public class Range {
+    public static void main(String[] args) {
+        Byte b = 10;
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+
+    }
+}
