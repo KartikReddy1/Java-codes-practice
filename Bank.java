83ee53e7c1a0679113e9a6c9ebdfb3f24c1bc091
KartikReddy1 <skr.somu387@gmail.com>
2026-08-13 01:01:35 +0530

    Added Bank.java

---
+++ Bank.java
@@ -0,0 +1,10 @@
+public class Bank {
+    public static void GreetCustomer() {
+        System.out.println("Hi sir,welcome to the bank");
+        
+    }
+    public static void main(String[] args) {
+        GreetCustomer();
+    }
+    
+}
