359b3843467382409fcfd6332f6e8715ed66a814
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 22:50:06 +0530

    Added Relational.java

---
+++ Relational.java
@@ -0,0 +1,13 @@
+public class Relational {
+    public static void main(String[] args) {
+        int i = 30;
+        int j = 20;
+        System.out.println(i == j);
+        System.out.println(i < j);
+        System.out.println(i > j);
+        System.out.println(i <= j);
+        System.out.println(i >= j);
+        System.out.println(i != j);
+    }
+    
+}
--- Unary.java
+++ Unary.java
@@ -3,10 +3,10 @@ public class Unary {
     public static void main(String[] args) {
         int i = 30;
         int j = 20;
-        System.out.println(i);
-        System.out.println(++i);
-        System.out.println(i++);
-        System.out.println(i);
+        System.out.println(i);//30
+        System.out.println(++i);//31
+        System.out.println(i++);//31
+        System.out.println(i);//32
     }
     
 }
