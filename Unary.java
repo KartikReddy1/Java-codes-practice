021cdb155fbfa90786c496e0ce9e5d5391b21f5a
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 22:44:09 +0530

    added Unary.java

---
+++ Unary.java
@@ -0,0 +1,12 @@
+
+public class Unary {
+    public static void main(String[] args) {
+        int i = 30;
+        int j = 20;
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+    }
+    
+}
