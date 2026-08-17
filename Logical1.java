1f35f3ef4798569dbd45949e71d289319460ec7e
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 23:10:21 +0530

    Added Logical1.java

---
+++ Logical1.java
@@ -0,0 +1,13 @@
+public class Logical1 {
+    int i = 30;
+    int j = 30;
+    public static void main(String[] args) {
+
+        Logical1 logical1 = new Logical1();
+
+      
+        System.out.println(logical1.i <logical1.j||logical1.i == logical1.j);
+        System.out.println(logical1.i >logical1.j&&logical1.i == logical1.j);
+    }
+}
+ 
