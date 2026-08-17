83b1ed25670a614977a9254ea3cb81ff0c2a595d
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 22:55:55 +0530

    Added Logical.java

---
+++ Logical.java
@@ -0,0 +1,9 @@
+public class Logical {
+    public static void main(String[] args) {
+        int i = 30;
+        int j = 30;
+        System.out.println(i < j||i == j);
+         System.out.println(i >= j&&i == j);
+    }
+    
+}
