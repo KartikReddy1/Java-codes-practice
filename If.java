fa0a9068fa351b3f58563e2e6650cacba5666013
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 23:34:58 +0530

    Added If.java

---
+++ If.java
@@ -0,0 +1,9 @@
+public class If {
+    public static void main(String[] args) {
+        int i = 10;
+        if(i>0){
+            System.out.println("the given number is positive");
+        }
+    }
+    
+}
