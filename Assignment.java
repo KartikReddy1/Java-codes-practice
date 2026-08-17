e3f3441037774961a629480a94bfdde566a886b8
KartikReddy1 <skr.somu387@gmail.com>
2026-08-17 23:29:53 +0530

    Added Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment {
+    static int i =30;
+    static int j =20;
+     public static void main(String[] args) {
+        i += 5;
+        System.out.println(i);
+
+        
+    }
+    
+}
