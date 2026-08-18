af6cb5d548c982bac9adad264373fb68ca3d43f1
KartikReddy1 <skr.somu387@gmail.com>
2026-08-18 22:14:55 +0530

    Added Days.java

---
+++ Days.java
@@ -0,0 +1,33 @@
+public class Days {
+    public static void main(String[] args) {
+        int i = 4;
+        switch(i) {
+            case 1:
+                System.out.println("Monday,start of the week");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Staturday");
+                break;
+            case 7:
+                System.out.println("Sunday,Weekend");
+                break;
+            default:
+                 System.out.println("Default statement");
+                break;                           
+
+        }
+    }
+    
+}
