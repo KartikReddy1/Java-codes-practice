a341ba6d4d834d7faa31d125b2ffb30a882870c5
KartikReddy1 <skr.somu387@gmail.com>
2026-08-18 21:56:45 +0530

    Added Switch.java

---
+++ Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+    public static void main(String[] args) {
+        int i = 3;
+        switch(i){
+            case 1:
+               System.out.println("First statement");
+               break;
+            case 2:
+               System.out.println("Second statement");
+               break;
+            case 3:
+               System.out.println("Third statement");
+               break;
+            default:
+                System.out.println("Default statement");
+                break;
+
+
+        }
+    }
+    
+}
