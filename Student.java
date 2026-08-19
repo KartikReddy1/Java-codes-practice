a2056ec12a94321ee33f702bb185589af513f457
KartikReddy1 <skr.somu387@gmail.com>
2026-08-19 23:46:54 +0530

    Added Student.java

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Karthik", 20);
+        s1.display();
+    }
+}
