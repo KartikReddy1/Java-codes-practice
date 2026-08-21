c1c878baec31df448bc5014dafdcd71c6dcec8c4
KartikReddy1 <skr.somu387@gmail.com>
2026-08-21 23:28:55 +0530

    Added Constructorpractice.java

---
+++ Constructorpractice.java
@@ -0,0 +1,7 @@
+public class Constructorpractice {
+    public static void main(String[] args) {
+         Constructorpractice cp = new  Constructorpractice();
+         System.out.println("how are you");
+    }
+    
+}
