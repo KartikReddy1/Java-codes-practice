6216f3f96bae8f0428e7a3e72122c28e532bada0
KartikReddy1 <skr.somu387@gmail.com>
2026-08-25 22:47:21 +0530

    Added Return.java

---
+++ Return.java
@@ -0,0 +1,16 @@
+public class Return {
+
+    static int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(10, 20);
+
+        System.out.println("Result = " + result);
+
+    }
+}
