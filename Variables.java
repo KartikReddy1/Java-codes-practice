709b0cb652bf49c3252f5be2b1d89b639c3100b5
KartikReddy1 <skr.somu387@gmail.com>
2026-08-27 22:50:32 +0530

    Added Variables.java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 50000.50;
+        char grade = 'A';
+        String name = "Karthik";
+        boolean pass = true;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(name);
+        System.out.println(pass);
+    }
+}
