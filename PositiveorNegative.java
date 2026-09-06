b895ed0edd10533fb6d62195cb3625c38be63e2f
KartikReddy1 <skr.somu387@gmail.com>
2026-09-06 22:51:22 +0530

    PositiveorNegative.java

---
+++ PositiveorNegative.java
@@ -0,0 +1,11 @@
+public class PositiveorNegative {
+    public static void main(String[] args) {
+        int n = -5;
+
+        if (n > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
