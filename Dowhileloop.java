2fe0dab0a8c97fdbeaa137e34278ba0eb841bdb4
KartikReddy1 <skr.somu387@gmail.com>
2026-09-09 00:39:56 +0530

    Added DoWhileloop.java

---
+++ Dowhileloop.java
@@ -0,0 +1,14 @@
+public class Dowhileloop {
+   static  int i = 0;
+    public static void main(String[] args) {
+        do{
+            
+        System.out.println(i);
+        i++;
+    
+    }
+    while(i<0);
+    
+}
+}
+
