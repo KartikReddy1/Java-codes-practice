8bb8b1cc44448f5d3d056392459b85c03c89457d
KartikReddy1 <skr.somu387@gmail.com>
2026-09-23 20:47:34 +0530

    Added SingleDimentionArray.java

---
+++ SingleDimentionArray.java
@@ -0,0 +1,18 @@
+public class SingleDimentionArray {
+    public static void main(String[] args) {
+        int[] j = new int[5];
+        j[0] = 55;
+        j[1] = 44;
+        j[2] = 33;
+        j[3] = 22;
+        j[4] = 11;
+       // System.out.println(j[2]);
+
+        for (int i = 0; i<j.length;i++){
+            System.out.println(j[i]);
+
+
+        }
+    }
+    
+}
