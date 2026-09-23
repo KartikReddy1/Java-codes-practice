b25312825f721f103146c2e5580733346ffa4e10
KartikReddy1 <skr.somu387@gmail.com>
2026-09-23 21:08:16 +0530

    Added Arraysum.java

---
+++ Arraysum.java
@@ -0,0 +1,16 @@
+public class Arraysum {
+    public static void main(String[] args) {
+        int[] j = new int[5];
+        j[0] = 2;
+        j[1] = 5;
+        j[2] = 1;
+        j[3] = 9;
+        j[4] = 6;
+        // for (int i = 0; i<j.length;i++)
+        // System.out.println(j[i]);
+        int sum = j[0] + j[1] + j[2] + j[3] + j[4] ;
+        System.out.println(sum);
+
+    }
+    
+}
