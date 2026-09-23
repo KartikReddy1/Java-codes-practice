e66bd52c408255a2bc94e8371855c9313fea72dc
KartikReddy1 <skr.somu387@gmail.com>
2026-09-23 22:15:59 +0530

    Added MergeArrays.java

---
+++ MergeArrays.java
@@ -0,0 +1,26 @@
+public class MergeArrays {
+    public static void main(String[] args) {
+        String[] first = {"chai", "coffee"};
+        String[] second = {"milk", "water", "coke"};
+
+         String[] merged = new String[first.length + second.length];
+
+         for (int i = 0; i < first.length; i++) {
+         merged[i] = first[i];
+        }
+         for (int i = 0; i < second.length; i++) {
+        merged[first.length+i]=second [i];
+         }
+       System.out.print("[");
+         for (int i = 0; i < merged.length; i++) {
+        System.out.print("\"" + merged[i] + "\"");
+        if (i < merged.length - 1) { 
+            System.out.print(", "); 
+        }
+     }
+             System.out.println("]"); 
+    } 
+
+
+    }
+
