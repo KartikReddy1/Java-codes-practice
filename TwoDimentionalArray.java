9fa8a6ac5987fcb893f86788a6be0130d28cd148
KartikReddy1 <skr.somu387@gmail.com>
2026-09-26 00:33:04 +0530

    Added TwoDimentionalArray.java

---
+++ TwoDimentionalArray.java
@@ -0,0 +1,25 @@
+public class TwoDimentionalArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][2];
+       arr [0][0] = 1;
+       arr [0][1] = 2;
+
+       arr [1][0] = 3;
+       arr [1][1] = 4;
+
+       arr [2][0] = 5;
+       arr [2][1] = 6;
+
+    //    System.out.println(arr[0][0]);
+    //    System.out.println(arr[0][1]);
+
+    for (int i =0;i < arr.length; i++){
+    int [] singleRow = arr [i];
+       for(int j =0; j< singleRow.length;j++){
+       System.out.print(singleRow[j]+"  ");
+    }
+     System.out.println();
+    }
+    
+}
+}
